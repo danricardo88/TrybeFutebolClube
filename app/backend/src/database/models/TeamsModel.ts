82aadea9a142ebbb56eb2a8e1eb5674442f7e776
danricardo88 <daniel.ricardo88@outlook.com>
2023-01-26 00:16:04 -0300

    criando matches e teams

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,33 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+import Matches from './MatchesMod';
+
+class Teams extends Model {
+  declare id:number;
+  declare teamName:string;
+}
+
+Teams.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    teamName: {
+      type: STRING(100),
+      allowNull: false,
+    } },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'teams',
+    timestamps: false,
+  },
+);
+
+Teams.hasMany(Matches);
+Matches.belongsTo(Teams);
+
+export default Teams;

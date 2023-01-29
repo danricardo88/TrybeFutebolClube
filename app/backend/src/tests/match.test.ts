1171915f40c75bd725373e75a790d4f59881957b
danricardo88 <daniel.ricardo88@outlook.com>
2023-01-29 05:20:43 -0300

    arrumando os tests... agora vai

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -17,7 +17,7 @@ const { app } = new App();
 const { expect } = chai;
 
 describe('"/matches" testes de integração de rota', () => {
-  let chaiHttpResp: Response;
+  let chaiHttpRes: Response;
 
   describe('GET', () => {
     afterEach(() => {
@@ -25,26 +25,26 @@ describe('"/matches" testes de integração de rota', () => {
     });
     it('Retorna todas as match', async () => {
       sinon.stub(Match, 'findAll').resolves(matchMock as IMatchDB[]);
-      chaiHttpResp = await chai.request(app).get('/matches');
+      chaiHttpRes = await chai.request(app).get('/matches');
 
-      expect(chaiHttpResp.status).to.be.equal(200);
-      expect(chaiHttpResp.body).to.deep.equal(matchMock);
+      expect(chaiHttpRes.status).to.be.equal(200);
+      expect(chaiHttpRes.body).to.deep.equal(matchMock);
     });
 
     it('Retorna match que estão em andamento', async () => {
       sinon.stub(Match, 'findAll').resolves([matchMock[1]] as IMatchDB[]);
-      chaiHttpResp = await chai.request(app).get('/matches?inProgress=true');
+      chaiHttpRes = await chai.request(app).get('/matches?inProgress=true');
 
-      expect(chaiHttpResp.status).to.be.equal(200);
-      expect(chaiHttpResp.body).to.deep.equal([matchMock[1]]);
+      expect(chaiHttpRes.status).to.be.equal(200);
+      expect(chaiHttpRes.body).to.deep.equal([matchMock[1]]);
     });
 
     it('Retorna match que não estão em andamento', async () => {
       sinon.stub(Match, 'findAll').resolves([matchMock[0]] as IMatchDB[]);
-      chaiHttpResp = await chai.request(app).get('/matches?inProgress=false');
+      chaiHttpRes = await chai.request(app).get('/matches?inProgress=false');
 
-      expect(chaiHttpResp.status).to.be.equal(200);
-      expect(chaiHttpResp.body).to.deep.equal([matchMock[0]]);
+      expect(chaiHttpRes.status).to.be.equal(200);
+      expect(chaiHttpRes.body).to.deep.equal([matchMock[0]]);
     });
   });
 
@@ -58,13 +58,13 @@ describe('"/matches" testes de integração de rota', () => {
           .stub(Match, 'create')
           .resolves(newMatchResp as IMatchDB);
 
-        chaiHttpResp = await chai
+        chaiHttpRes = await chai
           .request(app)
           .post('/matches')
           .send(newMatch)
           .auth('token', { type: 'bearer' });
-        expect(chaiHttpResp.status).to.be.equal(201);
-        expect(chaiHttpResp.body).to.deep.equal(newMatchResp);
+        expect(chaiHttpRes.status).to.be.equal(201);
+        expect(chaiHttpRes.body).to.deep.equal(newMatchResp);
       });
     });
 
@@ -72,37 +72,37 @@ describe('"/matches" testes de integração de rota', () => {
       it('Fails if the homeTeam is equal to awayTeam', async () => {
         (jwt.verify as sinon.SinonStub).restore();
         sinon.stub(jwt, 'verify').resolves({ id: 1 });
-        chaiHttpResp = await chai
+        chaiHttpRes = await chai
           .request(app)
           .post('/matches')
           .send(invalidMatch[0])
           .auth('token', { type: 'bearer' });
-        expect(chaiHttpResp.status).to.be.equal(422);
-        expect(chaiHttpResp.body).to.deep.equal({
+        expect(chaiHttpRes.status).to.be.equal(422);
+        expect(chaiHttpRes.body).to.deep.equal({
           message: 'It is not possible to create a match with two equal teams',
         });
       });
       it('Fails if a team does not exists', async () => {
         (jwt.verify as sinon.SinonStub).restore();
         sinon.stub(jwt, 'verify').resolves({ id: 1 });
-        chaiHttpResp = await chai
+        chaiHttpRes = await chai
           .request(app)
           .post('/matches')
           .send(invalidMatch[1])
           .auth('token', { type: 'bearer' });
-        expect(chaiHttpResp.status).to.be.equal(404);
-        expect(chaiHttpResp.body).to.deep.equal({
+        expect(chaiHttpRes.status).to.be.equal(404);
+        expect(chaiHttpRes.body).to.deep.equal({
           message: 'There is no team with such id!',
         });
       });
       it('Fails if the token is invalid', async () => {
         (jwt.verify as sinon.SinonStub).restore();
-        chaiHttpResp = await chai
+        chaiHttpRes = await chai
           .request(app)
           .post('/matches')
           .send(newMatch);
-        expect(chaiHttpResp.status).to.be.equal(401);
-        expect(chaiHttpResp.body).to.deep.equal({
+        expect(chaiHttpRes.status).to.be.equal(401);
+        expect(chaiHttpRes.body).to.deep.equal({
           message: 'Token must be a valid token',
         });
       });
@@ -111,7 +111,7 @@ describe('"/matches" testes de integração de rota', () => {
 });
 
 describe('"/matches/:id/finish" route integration tests', () => {
-  let chaiHttpResp: Response;
+  let chaiHttpRes: Response;
 
   describe('PATCH', () => {
     afterEach(() => {
@@ -121,19 +121,19 @@ describe('"/matches/:id/finish" route integration tests', () => {
     it('Finishs a match by his id', async () => {
       sinon.stub(Match, 'update').resolves([1]);
 
-      chaiHttpResp = await chai.request(app).patch('/matches/1/finish');
+      chaiHttpRes = await chai.request(app).patch('/matches/1/finish');
 
-      expect(chaiHttpResp.status).to.be.equal(StatusCodes.OK);
-      expect(chaiHttpResp.body).to.deep.equal({ message: 'Finished' });
+      expect(chaiHttpRes.status).to.be.equal(StatusCodes.OK);
+      expect(chaiHttpRes.body).to.deep.equal({ message: 'Finished' });
     });
 
     it('Fails if the update goes wrong', async () => {
       sinon.stub(Match, 'update').resolves([-1]);
 
-      chaiHttpResp = await chai.request(app).patch('/matches/1/finish');
+      chaiHttpRes = await chai.request(app).patch('/matches/1/finish');
 
-      expect(chaiHttpResp.status).to.be.equal(StatusCodes.OK);
-      expect(chaiHttpResp.body).to.deep.equal({
+      expect(chaiHttpRes.status).to.be.equal(StatusCodes.OK);
+      expect(chaiHttpRes.body).to.deep.equal({
         message: 'Update unsuccessful',
       });
     });
